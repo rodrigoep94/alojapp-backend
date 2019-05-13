148fe38c9f14a559355b2e810c2663b1294a9f32
juanmanuelvallejos <juanmanuelvallejos2@gmail.com>
2019-05-12 21:26:30 -0300

    Se agrega PUT a CORS de controller

--- src/main/java/com/grupo1/alojapp/Controllers/AlojamientoController.java
+++ src/main/java/com/grupo1/alojapp/Controllers/AlojamientoController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST, RequestMethod.PUT })
 public class AlojamientoController {
 
     @Autowired
